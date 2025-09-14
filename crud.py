246f1b2dc761a436ef61a2ce0d907d680e8bc780
Bittner89 <bittner-89@web.de>
2025-09-14 12:48:47 +0200

    Created a function that creates an employee entry.

---
+++ crud.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from database import EmployeeDB
+from models import EmployeeCreate, EmployeeUpdate
+from datetime import datetime
+
+# Create an employee
+
+def create_employee(db: Session, employee: EmployeeCreate):
+    # check if email exists
+    existing = db.query(EmployeeDB).filter(EmployeeDB.email == employee.email).first()
+    if existing:
+        raise ValueError("Email bereits vergeben.")
+    
+    # create a new entry for employee
+
+    db_employee = EmployeeDB(
+        name = employee.name,
+        email = employee.email,
+        position = employee.position,
+        start_date = employee.start_date,
+        salary = employee.salary,
+        created_at = datetime.utcnow()
+    )
+    try:
+        db.add(db_employee)
+        db.commit()
+        db.refresh(db_employee)
+    except IntegrityError:
+        db.rollback()
+        raise ValueError("Fehler beim speichern")    
+ 
