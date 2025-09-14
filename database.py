6b70969c1745647e0afbd2234f517bb6173bae18
Bittner89 <bittner-89@web.de>
2025-09-14 13:45:53 +0200

    Created a /post endpoint for creating an employee.

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, float, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
-app = FastAPI()
+
+from database import get_db
+from models import Employee, EmployeeCreate, EmployeeUpdate
+import crud
+
+app = FastAPI(
+    title = "Employee Management API",
+    description = "Adding, reading, updating and deleting employees from Postgres DB.",
+    version = "1.0" 
+)
 
 @app.get("/health")
 def health_check():
     return {"status":"all good and dandy"}
+
+@app.post("/employees",response_model=Employee, status_code=status.HTTP_201_CREATED)
+def create_employee_endpoint(employee: EmployeeCreate, db: Session = Depends(get_db)):
+    try:
+        db_employee = crud.create_employee(db = db, employee = employee)
+        return db_employee
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
